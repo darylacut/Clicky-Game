f306ac5f7cde35e1b6f1d3fd0a6b86eeb79be676
Daryl Acut <darylacut@yahoo.com>
2019-09-09 03:56:03 -0400

    created CSS styles and fixed alignment

--- src/App.js
+++ src/App.js
@@ -5,15 +5,33 @@ import Wrapper from "./components/Wrapper";
 import places from "./places.json";
 import './App.css';
 
-function App() {
-  return (
-    <div className="container">
-       
-        <Jumbotron/>
-        <Card/>
-       
+
+class App extends Component {
+  // Setting this.state.friends to the friends json array
+  state = {
+    places
+  };
+
+
+  // Map this state of places and render a Card component for each object
+  render() {
+    return (
+    <div>
+      <Jumbotron/>
+      <Wrapper>
+         {this.state.places.map(place => (
+          <Card
+            id={place.id}
+            name={place.name}
+            image={place.image}
+            country={place.country}
+          />
+         
+        ))}
+      </Wrapper>
     </div>
   );
+}
 }
 
 export default App;
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -3,15 +3,19 @@ import "./style.css";
 
 function Card(props) {
   return (
-    <div className="card bg-dark text-white">
-        <img className="card-img" src={props.image} alt={props.name}/>
-        <div className="card-img-overlay">
-            <h5 className="card-title">{props.name}</h5>
-            <p className="card-text">{props.country}</p>
-        </div>
+    <div className="deck">
+      <div className="card bg-dark text-white">
+          <img className="card-img" src={props.image} alt={props.name}/>
+          <div className="card-img-overlay">
+              <h5 className="card-title">{props.name}</h5>
+              <p className="card-text">{props.country}</p>
+          </div>
+      </div>
     </div>
+
   );
 }
+
 
 export default Card;
 
