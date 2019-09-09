b58ff8319ec3437a74109255051a9ab92a1d0b96
Daryl Acut <darylacut@yahoo.com>
2019-09-08 23:53:52 -0400

    added card places json

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Jumbotron from "./components/Jumbotron";
+import Card from "./components/Card";
+import places from "./places.json";
 import './App.css';
 
 function App() {
   return (
     <div className="container">
        
-        <Jumbotron />
+        <Jumbotron/>
+        <Card/>
        
     </div>
   );
---
+++ src/components/Card.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Card(props) {
+  return (
+    <div className="card bg-dark text-white">
+        <img className="card-img" src={props.image} alt={props.name}/>
+        <div className="card-img-overlay">
+            <h5 className="card-title">{props.name}</h5>
+            <p className="card-text">{props.country}</p>
+        </div>
+    </div>
+  );
+}
+
+export default Card;
+
+
