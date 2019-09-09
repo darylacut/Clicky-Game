c85f25e88698b189ca38081be59cf7af2a22f9a7
Daryl Acut <darylacut@yahoo.com>
2019-09-09 00:39:06 -0400

    changed file directory tree

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Jumbotron from "./components/Jumbotron";
-import Card from "./components/Card";
+import Card from "./components/Card/";
+import Wrapper from "./components/Wrapper";
 import places from "./places.json";
 import './App.css';
 
